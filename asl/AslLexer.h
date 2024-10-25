dc6fde9e6534ea94bf10878456b55443e31334d2
xennsss <100310274+xennsss@users.noreply.github.com>
2024-10-25 16:00:20 +0200

    Add files via upload

---
+++ asl/AslLexer.h
@@ -0,0 +1,61 @@
+
+// Generated from Asl.g4 by ANTLR 4.9.2
+
+#pragma once
+
+
+#include "antlr4-runtime.h"
+
+
+
+
+class  AslLexer : public antlr4::Lexer {
+public:
+  enum {
+    T__0 = 1, T__1 = 2, T__2 = 3, T__3 = 4, ASSIGN = 5, EQUAL = 6, PLUS = 7, 
+    MUL = 8, VAR = 9, INT = 10, BOOL = 11, FLOAT = 12, CHAR = 13, IF = 14, 
+    THEN = 15, ELSE = 16, ENDIF = 17, FUNC = 18, ENDFUNC = 19, READ = 20, 
+    WRITE = 21, ID = 22, INTVAL = 23, CHARVAL = 24, FLOATVAR = 25, AMB_PUNT = 26, 
+    SENSE_PUNT = 27, SIMBOL = 28, DIGIT = 29, STRING = 30, COMMENT = 31, 
+    WS = 32
+  };
+
+  explicit AslLexer(antlr4::CharStream *input);
+  ~AslLexer();
+
+  virtual std::string getGrammarFileName() const override;
+  virtual const std::vector<std::string>& getRuleNames() const override;
+
+  virtual const std::vector<std::string>& getChannelNames() const override;
+  virtual const std::vector<std::string>& getModeNames() const override;
+  virtual const std::vector<std::string>& getTokenNames() const override; // deprecated, use vocabulary instead
+  virtual antlr4::dfa::Vocabulary& getVocabulary() const override;
+
+  virtual const std::vector<uint16_t> getSerializedATN() const override;
+  virtual const antlr4::atn::ATN& getATN() const override;
+
+private:
+  static std::vector<antlr4::dfa::DFA> _decisionToDFA;
+  static antlr4::atn::PredictionContextCache _sharedContextCache;
+  static std::vector<std::string> _ruleNames;
+  static std::vector<std::string> _tokenNames;
+  static std::vector<std::string> _channelNames;
+  static std::vector<std::string> _modeNames;
+
+  static std::vector<std::string> _literalNames;
+  static std::vector<std::string> _symbolicNames;
+  static antlr4::dfa::Vocabulary _vocabulary;
+  static antlr4::atn::ATN _atn;
+  static std::vector<uint16_t> _serializedATN;
+
+
+  // Individual action functions triggered by action() above.
+
+  // Individual semantic predicate functions triggered by sempred() above.
+
+  struct Initializer {
+    Initializer();
+  };
+  static Initializer _init;
+};
+
